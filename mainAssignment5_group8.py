24196bfc2c7267a3ee5780f63bc266faf7d6f020
Thommy1098 <128376721+Thommy1098@users.noreply.github.com>
2023-03-20 13:48:16 +0100

    Update mainAssignment5_group8.py

--- mainAssignment5_group8.py
+++ mainAssignment5_group8.py
@@ -1,2 +1,12 @@
-ciao
-tutto bene
+#from pandas import pd
+import pandas as pd
+df= pd.read_csv('EUROSTOXX50_2023_Dataset.csv') #, index_cd=0)
+df.fillna(axis=0,method='ffill',inplace=True)
+#print(df)
+print(df['Date'])
+print(df['AD.AS'])
+
+from DateTime import DateTime
+y = DateTime('Mar 7, 2013')
+print(y)
+df['AD.AS'].index(29.735000)
