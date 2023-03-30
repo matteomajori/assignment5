ffd17b10f1150c2c9a3969fe42887ff95c3f3dfc
Vanessa <128375168+vanessatenconi@users.noreply.github.com>
2023-03-30 14:53:41 +0200

    update

--- utilities.py
+++ utilities.py
@@ -61,51 +61,58 @@ def WHSMeasurements(returns, alpha, lambd, weights, portfolioValue, riskMeasureT
     w_desce = w[index_w].T
     #We look for i_star: the largest value s.t. sum(w_i, i=1,..,i_star)<=1-alpsha
     i_star = np.where(np.cumsum(w_desce) <= 1 - alpha)[0][-1]+1
-    
-
+    #compute VaR
     VaR = riskMeasureTimeIntervalInDay*Loss_desce[0,i_star]
+    #compute ES
     ES = riskMeasureTimeIntervalInDay*(np.sum(w_desce[0,0:i_star]*Loss_desce[0,0:i_star])/np.sum(w_desce[0,0:i_star]))
     return VaR, ES
 ##c
 def PrincCompAnalysis(yearlyCovariance, yearlyMeanReturns, weights, H, alpha, numberOfPrincipalComponents, portfolioValue):
     eval, evect = np.linalg.eig(yearlyCovariance)
-
+    #Compute VaR and ES using the Principal Component Analysis method
     #order eigenvalues in descending way
     eval_desce = np.sort(eval).T[::-1]
+    #find the indexes of the descending loss
     eval_index = np.argsort(eval).T[::-1]
     #reordering the eigenvectors
     evect=evect[:,eval_index]
+    #reordering the returns using the index
     yearlyMeanReturns=yearlyMeanReturns[eval_index]
 
-    #reduced form portfolio
+    #define reduced form portfolio
     mu_hat =np.dot(evect.T , yearlyMeanReturns.T)
     w_hat = np.dot(evect.T , weights.T)
 
-    #computing mean and variance of the reduced ptf up to K
+    #computing mean and variance of the reduced ptf up to number of components K
     k=numberOfPrincipalComponents
+    #compute mean and variance of the reduced portfolio
     Mean_reduced = -np.sum(mu_hat[0:k]*w_hat[0: k]) *H  #mu_red * delta
     Sigma_reduced = np.sqrt(np.sum(eval_desce[0:k]*np.square(w_hat[0:k]))*H) #sqrt(sigma_red*delta)
-
+    #compute VaR
     VaR = portfolioValue * (Mean_reduced+Sigma_reduced * norm.ppf(alpha)) #mu_red * delta +  sigma_red * sqrt(delta) * VaR_std
+    #compute ES
     ES = portfolioValue * (Mean_reduced+Sigma_reduced*  norm.pdf(norm.ppf(alpha)) / (1 - alpha)) #mu_red * delta +   sqrt(sigma_red *delta) * ES_std
 
     return VaR, ES
 
 def plausibilityCheck(returns, portfolioWeights, alpha, portfolioValue, riskMeasureTimeIntervalInDay):
-    # estimation of the order of magnitude of portfolio VaR
+    # Estimation of the order of magnitude of portfolio VaR
     C = riskMeasureTimeIntervalInDay*np.corrcoef(returns.T,rowvar=False) #correlation matrix
 
     u = np.quantile(returns, alpha, axis=1) #upper quantile along axis 1 (i.e row-wise)
     l = np.quantile(returns, (1 - alpha), axis=1) #lower quantile along axis 1 (i.e row-wise)
+    #compute sensitivities
     sens=-portfolioValue * portfolioWeights
-    #sVaR = portfolioValue* portfolioWeights * (abs(l) + abs(u))/ 2  #signed-VaR
+    #compute sign VaR as sVaR = portfolioValue* portfolioWeights * (abs(l) + abs(u))/ 2
     sVaR = sens * (abs(l) + abs(u))/ 2  #signed-VaR
+    #compute VaR
     VaR = np.sqrt(np.sum(np.dot(sVaR,C)*sVaR)) #*portfolioValue
 
     return VaR
 
 def FullMonteCarloVaR(logReturns, numberOfShares, numberOfPuts, stockPrice, strike, rate, dividend, volatility,
                           timeToMaturityInYears, riskMeasureTimeIntervalInYears, alpha, NumberOfDaysPerYears):
+    #compute VaR using Full MOnteCarlo Valuation method
     M = 10 ** 6
     put_price = PutPrice(rate,stockPrice,strike,dividend,volatility,timeToMaturityInYears)
     # Random indexes: to check
@@ -190,14 +197,14 @@ def CliquetPrice_numerical(volatility,StockPrice,SurvProb,discounts,rates,recove
     #inizializations
     s=np.zeros((M,4))
     payoff=np.zeros((M,4))
-    #simulated stock 
-    s[:,0]= StockPrice *  np.exp(rates[0] -0.5 * volatility**2 + volatility*u[:,0])
-
-
     #compute S_ti and the payoff of the cliquet
+
+    #simulated stock at the first year using the first column of the random set
+    s[:,0]= StockPrice *  np.exp(rates[0] -0.5 * volatility**2 + volatility*u[:,0])
+    #cumulative sum over years
     u_cumsum=np.cumsum(u,axis=1)
-    s = StockPrice * np.exp((rates - 0.5 * volatility ** 2) * np.arange(1,5) + volatility * u_cumsum)# * np.sqrt( np.arange(1,5)))
-    payoff[:,0] = np.maximum(s[:, 0] - StockPrice, 0)
+    #simulated stock at the others years using the cumulative s
+    s = StockPrice * np.exp((rates - 0.5 * volatility ** 2) * np.arange(1,5) + volatility * u_cumsum)
     payoff[:,1:] = np.maximum(s[:, 1:] - s[:, :-1], 0)
     
 
