f16f0790591e7abe35e869870c1c0222ac4fa33f
pietroloro00 <128376743+pietroloro00@users.noreply.github.com>
2023-03-31 09:27:51 +0200

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -222,8 +222,8 @@ def CliquetPrice(volatility,StockPrice,SurvProb,discounts,rates,recovery):
     payoff[:, 0] = np.maximum(s[:, 0] - StockPrice, 0)
     payoff[:,1:] = np.maximum(s[:, 1:] - s[:, :-1], 0)
     
-    #compute the correct price of the cliquet
-    Cliquet = np.sum((np.mean(payoff,axis=0)*discounts)*SurvProb[1:]) +recovery * np.sum(np.mean(payoff,axis=0)*discounts*(SurvProb[:- 1]-SurvProb[1:]))
+    #compute the correct price of the cliquet, we suppose that the recovery of an exotic option is zero because of seniority
+    Cliquet = np.sum((np.mean(payoff,axis=0)*discounts)*SurvProb[1:]) #+recovery * np.sum(np.mean(payoff,axis=0)*discounts*(SurvProb[:- 1]-SurvProb[1:]))
     #compute the price with no risk of default
     Cliquet_riskfree = np.sum((np.mean(payoff,axis=0)*discounts))
 
